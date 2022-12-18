5e018ea237606baccb178cc360e9696db9b68f81
hustada <mark.hustad@protonmail.com>
2022-12-18 12:45:51 -0600

    comments

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
+// Book component that displays a book and allows the user to move it to a different shelf
 const Book = ({ book, updateBook, shelfTitle }) => {
+  // If the book has imageLinks defined and a thumbnail, set the thumbnail variable. Otherwise, set it to an empty string.
   const thumbnail = book.imageLinks ? book.imageLinks.thumbnail : "";
 
+  // Function to be called when the user selects a new shelf for the book
   const handleChange = (e) => {
+    // Call the updateBook function, passing in the book and the selected shelf value
     updateBook(book, e.target.value)
   }
 
+  // Array of shelf objects, containing the shelf name and display name
   const shelves = [
     {id: "1", shelfName: "currentlyReading", shelfDisplayName: "Currently Reading"},
     {id: "2", shelfName: "wantToRead", shelfDisplayName: "Want to Read"},
@@ -15,6 +20,7 @@ const Book = ({ book, updateBook, shelfTitle }) => {
     {id: "4", shelfName: "none", shelfDisplayName: "none"}
   ]
 
+  // Render the book component
   return (
     <div className="book">
       <div className="book-top">
@@ -27,11 +33,13 @@ const Book = ({ book, updateBook, shelfTitle }) => {
           }}
         ></div>
         <div className="book-shelf-changer">
+          {/* Render a select element with options for each shelf, and bind the handleChange function to the onChange event */}
           <select
             onChange={handleChange}
             defaultValue={book.shelf}
           >
             <option value="move" disabled>Move to...</option>
+            {/* Map over the shelves array, rendering an option element for each shelf */}
             {shelves.map((shelf, index) => {
               return(
               <option key={index} value={shelf.shelfName}>
@@ -41,16 +49,19 @@ const Book = ({ book, updateBook, shelfTitle }) => {
           </select>
         </div>
       </div>
+      {/* Render the book title */}
       <div className="book-title">{book.title}</div>
+      {/* Render the book authors */}
       <div className="book-authors">{book.authors}</div>
     </div>
   )
 }
 
+// Define the prop types for the Book component
 Book.propTypes = {
   book: PropTypes.object.isRequired,
   updateBook: PropTypes.func
 };
 
-
+// Export the Book component
 export default Book
--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom';
 
 const BookDetails = ({updateBook}) => {
+  // Access the current location in the application
   const location = useLocation();
+  // Extract the book data from the location state
   const book = (location.state.data);
+  // Extract the book details from the book object
   const title = book.title;
   const author = book.authors
   const category = book.categories;
@@ -11,10 +14,13 @@ const BookDetails = ({updateBook}) => {
   const pageCount = book.pageCount;
   const publisher = book.publisher;
   const publishedDate = book.publishedDate;
+  // Extract the thumbnail image URL from the book object, or set it to an empty string if it is not available
   const thumbnail = book.imageLinks ? book.imageLinks.thumbnail : ""
   const shelf = book.shelf;
 
+  // Define a function to handle changes to the shelf dropdown menu
   const handleChange = (e) => {
+    // Call the updateBook function with the book object and the selected shelf value as arguments
     updateBook(book, e.target.value)
   }
 
@@ -27,12 +33,16 @@ const BookDetails = ({updateBook}) => {
           style={{
             width: 128,
             height: 193,
+            // Set the background image of the div to the thumbnail URL
             backgroundImage: `url(${thumbnail})`,
           }}
         ></div>
          <div className="book-shelf-changer">
+          {/* Render a dropdown menu to select the shelf of the book */}
           <select
+            // Set the default value of the dropdown menu to the current shelf of the book
             defaultValue={book.shelf}
+            // Call the handleChange function when the dropdown menu value changes
             onChange={handleChange}
           >
             <option value="none">
@@ -48,28 +58,37 @@ const BookDetails = ({updateBook}) => {
         </div>
       </div>
     </div>
+      {/* Render a div element to display the book details */}
       <div>
+        {/* Render a heading element to display the title of the book */}
         <h2>
           <strong>Title:</strong> {title}
         </h2>
+        {/* Render a paragraph element to display the authors of the book */}
         <p>
           <strong>Authors:</strong> {author}
         </p>
+        {/* Render a paragraph element to display the categories of the book */}
         <p>
           <strong>Category:</strong> {category}
         </p>
+        {/* Render a paragraph element to display the published date of the book */}
         <p>
           <strong>Published Date:</strong> {publishedDate}
         </p>
+        {/* Render a paragraph element to display the publisher of the book */}
         <p>
           <strong>Publisher:</strong> {publisher}
         </p>
+        {/* Render a paragraph element to display the page count of the book */}
         <p>
           <strong>Page Count:</strong> {pageCount}
         </p>
+        {/* Render a paragraph element to display the shelf of the book */}
         <p>
           <strong>Shelf:</strong> {shelf}
         </p>
+        {/* Render an italicized element to display the description of the book */}
         <i className='book-description'>{description}</i>
       </div>
       <Link className='details-back-link' to={'/'}>Back</Link>
